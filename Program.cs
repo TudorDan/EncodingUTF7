a849fe5544c6146f6507b72c6558e1aaf406bf2e
TudorDan <tudor.ist@gmail.com>
2020-10-05 20:43:56 +0300

    Add different encoding type

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace EncodingUTF7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream theFile = File.Create("C:/Users/antoaneta/Downloads/CodeCool/advancedCSharp/2nd_TW/EncodingUTF7/gitignore_utf7.txt");
+            StreamWriter writer = new StreamWriter(theFile, Encoding.UTF7, 512);
+
+            writer.Write("Aut viam inveniam aut faciam: 24*05*1982!");
+
+            writer.Close();
+            theFile.Close();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
